378b75e16539641f4edae8f6818bae10d91937e8
arasu <arasumurugan1903@gmail.com>
2024-12-28 11:29:41 +0530

    operator

---
+++ operator.js
@@ -0,0 +1,20 @@
+//1.Assignment operator
+a=10
+console.log(a);
+
+//2.Uranary Operator
+a++;
+console.log(a);
+
+//3. Arithmetic Operator
+a=a+a;
+console.log(a);
+
+//4.Logical operator
+
+var log1=14;
+var log2=15;
+console.log(log1 && log2);
+
+//5.Bitwise operator
+
