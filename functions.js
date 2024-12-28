4c279257fe089aa9d1a9fa871e65cb15f7835afd
arasu <arasumurugan1903@gmail.com>
2024-12-28 20:18:17 +0530

    function

---
+++ functions.js
@@ -0,0 +1,72 @@
+/*function kec(studentsdept){
+    console.log(studentsdept);
+}
+console.log(kec['CSE','IT','AIML']);
+
+//ArrowFuction
+arrowfun = (studentsdept) => {
+    return studentsdept(0);
+}
+console.log(arrowfun['AIML','IT','CSE','CSE','IT','AIML']);
+
+
+var marks = [90,95,93]
+var [a1,a2,a3]= marks
+console.log(a1,a2,a3);
+
+//Ternary Operator
+
+a = 11;
+out = (a%2==0) ? "EVen" : "Odd"
+console.log(a,"is",out)
+
+//Spread Operator
+
+arr1 = [10,20,30]
+arr2 = [...arr1 ,40,50]
+console.log(arr2);
+
+
+//LocalScope
+var a =5;
+let b=3.14;
+console.log(a)
+console.log(b)
+{
+    a =10;
+    b=5;
+    console.log(a)
+    console.log(b)
+
+}
+console.log(a)
+console.log(b)
+
+//Hoisting
+
+console.log(a)
+var a = 10;
+console.log(a)
+
+//undefined -- output
+//10
+console.log(a)
+var a =10
+console.log(a)
+
+//error
+
+console.log(a)
+const a =10
+console.log(a)
+
+//error
+console.log(a)
+let a =10
+console.log(a)
+*/
+
+
+
+
+
